221a9ef24c502ba93ff51c755a713f62ad89eae0
stevenfot <sdi2100209@di.uoa.gr>
2024-10-17 17:50:52 +0300

    Euclidean Distance

---
+++ lib/utils.hpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <cmath>
+using namespace std;
+
+// Function for the calculation of the Eycledian distance
+
+template<typename Type>             // Type is either integer or float
+float Euclidean_Distance( vector<Type> a, vector<Type> b) {
+    
+    int sum = 0;
+
+    // Iterators of the vectors
+    auto i = a.begin();
+    auto j = b.begin();
+    
+    while( i != a.end() || j != b.end() ){
+        sum += pow(*j-*i,2);           // sum = (i1 -j1)^2 + ... + (i_n-j_n)^2
+        i++;
+        j++;
+    }
+    float x = sqrt(sum);                        // [(i1 -j1)^2 + ... + (i_n-j_n)^2] ^ 1/2
+    return x;
+
+}
