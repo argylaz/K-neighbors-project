156d67c5c8be37444af50c5f47d57b3c6431884e
argylaz <argyrhslazaridhs@gmail.com>
2024-12-08 23:53:14 +0200

    Extended get_arguments - changed read_queries to return a vector of query vectors and a vector of the equivalent filters

--- groundtruth.cpp
+++ groundtruth.cpp
@@ -1,38 +1,6 @@
 #include "lib/filterGraph.hpp"
-#include "lib/file_io.hpp"
 
 using namespace std;
- 
-
-/* Run with make groundtruth */
-
-
-void create_test_bin_files(){
-    // Create vector of vectors with data
-    vector<vector<float>> vectors = { 
-        /* F  |     |Data*/
-        {1.0f, 0.0f, 0.0f},                   // second entry is always 0 (timestamp that is ignored)
-        {4.0f, 0.0f, 1.0f},
-        {0.0f, 0.0f, 2.0f},
-        {1.0f, 0.0f, 3.0f},
-        {1.0f, 0.0f, 4.0f},
-        {1.0f, 0.0f, 5.0f},
-        {2.0f, 0.0f, 6.0f},
-        {2.0f, 0.0f, 7.0f},
-        {3.0f, 0.0f, 8.0f},
-        {3.0f, 0.0f, 9.0f},
-        {2.0f, 0.0f, 10.0f},
-    };
-
-    /* Create a .bin file with the expected format */
-    try {
-        make_bin("sift/test_data.bin", vectors);
-    } catch (const exception& e) {
-        cerr << "Error: " << e.what() << '\n';
-    }
-      
-}
-
 
 /*  The data structure of the groundtruth.bin file is explained below:
     Begins with an integer num_queries (uint32_t) indicating the number of queries which covers the groundtruth
@@ -51,7 +19,7 @@ void write_to_bin(ofstream& groundtruth_file, vector<gIndex>& k_nearests){
     cout << "Writing index vector of size " << k << endl; 
 
 
-    for( gIndex neighbor : k_nearests ){
+    for ( gIndex neighbor : k_nearests ) {
         // Write the neighbors index into the file
         groundtruth_file.write(reinterpret_cast<const char*>(&neighbor), sizeof(gIndex));
     }
@@ -68,7 +36,10 @@ void write_to_bin(ofstream& groundtruth_file, vector<gIndex>& k_nearests){
 int main(void) {
 
     // Read queries from dummy-queries.bin
-    map<vector<float>, float> queries_data = read_queries("sift/dummy-queries.bin", 100);  
+    pair<vector<vector<float>>, vector<float>> queries_data = read_queries("sift/dummy-queries.bin", 10000, 100);  
+    vector<vector<float>> queries = queries_data.first;
+    vector<float> queries_filter = queries_data.second;
+
 
     // Create a FilterGraph with the data from dummy-data.bin
     FilterGraph<vector<float>, float> G("sift/dummy-data.bin", 100, true);
@@ -90,24 +61,24 @@ int main(void) {
 
 
     // Write the number of queries - groudtruth
-    uint32_t N = (uint32_t) queries_data.size();
+    uint32_t N = (uint32_t) queries.size();
     groundtruth_file.write(reinterpret_cast<const char*>(&N), sizeof(N));
 
 
     // Initialising temp vector for the calculations
     set<gIndex> temp1;
-    for( vector<float> l : vertices){
+    for (vector<float> l : vertices) {
         temp1.insert(G.get_index_from_vertex(l));
     }
 
-    /* Repeatitively find neares neighbors for each query and write into groudtruth.bin */
-    for( auto it = queries_data.begin(); it != queries_data.end(); it++ ){
-
+    /* Repeatitively find nearest neighbors for each query and write into groudtruth.bin */
+    // for( auto it = queries_data.begin(); it != queries_data.end(); it++ ){
+    for ( size_t q = 0; q < queries.size(); q++ ) {
         // Set with all the gIndices
         set<gIndex> temp = temp1;
 
         // Erase the vertex itself from the search list (temp vector)
-        auto p = find(temp.begin(), temp.end(), G.get_index_from_vertex(it->first)); 
+        auto p = find(temp.begin(), temp.end(), G.get_index_from_vertex(queries[q])); 
         if ( p != temp.end() ) {                        // Only remove if it was found within temp
             temp.erase(p);
         }
@@ -117,25 +88,25 @@ int main(void) {
         vector<gIndex> k_nearests;
 
         cout << endl << k << " nearests of ";
-        print_vector(it->first);
+        print_vector(queries[q]);
         cout << endl << " with filter ";
-        cout << it->second;
+        cout << queries_filter[q];
         cout << " are:\n";
         
         // k nearest points, so k iterations
         int j = 0;
         int count = 0; 
-        while( count < k && j < n){
+        while ( count < k && j < n) {
             
             // Find the nearest each time 
-            vector<float> min = find_min_Euclidean<float>(G, temp, it->first); 
+            vector<float> min = find_min_Euclidean<float>(G, temp, queries[q]); 
 
             set<float> filters_min = G.get_filters(G.get_index_from_vertex(min));
             set<float> filter_temp;
-            filter_temp.insert(it->second);
+            filter_temp.insert(queries_filter[q]);
 
             // The point in question is one of the k-nearest neighbors only if has the same filter as the query
-            if( filter_temp == filters_min  || it->second == -1){       // If the query has no categorical attribute (== -1) then ignore the filter
+            if ( filter_temp == filters_min  || queries_filter[q] == -1) {       // If the query has no categorical attribute (== -1) then ignore the filter
                 // --------------
                 // print_vector(min);
                 // cout << " with filter ";
@@ -165,7 +136,6 @@ int main(void) {
 
     // Close groundtruth.bin
     groundtruth_file.close();
-
 
     return 0;
 
--- lib/file_io.hpp
+++ lib/file_io.hpp
@@ -27,12 +27,7 @@ set<vector<Type>> read_sets(string& filename);
 
 
 // Function that reads the command line input arguments. Returns 1 or -1
-int get_arguments(int argc, char* argv[], int& k, int& L, float& a, int& R, string& base_name, string& query_name, string& groundtruth_name, string& vamanaFlag);
-
-
-/* Creates an fvec file with the given vectors for testing */
-template <typename type>
-void make_vec(const string& filename, const vector<vector<type>>& vectors);
+int get_arguments(int argc, const char* argv[], int& k, int& L, float& a, int& R, string& data_set, string& base_name, string& query_name, string& groundtruth_name, string& vamana_type);
 
 
 // Helper method to check file extention
@@ -41,12 +36,36 @@ bool hasBinExtension(const string& filename);
 
 /* Reads queries from sift/dummy-queries.bin */
 /* Returns a map from query nodes to their floating type filter value (-1 for no filter) */
-map<vector<float>, float> read_queries(const string& filename, int num_queries);  // !!! Throws exception if file extention is not .bin (call with try/catch)
+// num_dimensions is only given for testing purposes, all the query datasets with filters have num_dimensions = 100 so that's the default value
+pair<vector<vector<float>>, vector<float>> read_queries(const string& filename, int num_queries, int num_dimensions=100);  // !!! Throws exception if file extention is not .bin (call with try/catch)
+
+
+// Return 1 if it succeed, else returns 0
+vector<vector<gIndex>> read_groundtruth(string filename);
+
+
+/* Creates an fvec file with the given vectors for testing */
+template <typename type>
+void make_vec(const string& filename, const vector<vector<type>>& vectors);
+
 
 /* Creates a .bin file with the given data vectors that starts with the number of vectors */
 template <typename T>
 void make_bin(const string& filename, const vector<vector<T>> vectors);
 
+
+/* Creates a .bin file with the given data vectors that starts with the number of vectors */
+template <typename T>
+void make_queries(const string& filename, const vector<vector<T>> vectors);
+
+
+/* Method to save the medoid map of the graph in a file <prefix>_medoid_map.bin */
+template <typename F>
+bool save_medoid_map_to_bin(string file_prefix, map<F, gIndex> medoid_map);
+
+/* Method to get the adjacency list of the graph from a file <prefix>_graph.bin */
+template <typename F>
+bool get_medoid_map_from_bin(string file_prefix, map<F, gIndex>& medoid_map);
 
 
 /*--------------------------------------Methods used in main.cpp-------------------------------------*/
@@ -208,9 +227,8 @@ set<vector<Type>> read_sets(string& filename) {
 
 
 
-
 // Function that reads the command line input arguments. Returns 1 or -1
-int get_arguments(int argc, const char* argv[], int& k, int& L, float& a, int& R, string& base_name, string& query_name, string& groundtruth_name, string& vamana_type){
+int get_arguments(int argc, const char* argv[], int& k, int& L, float& a, int& R, string& data_set, string& base_name, string& query_name, string& groundtruth_name, string& vamana_type) {
     // We need at least 8 arguments (Filename, k, L, R and maybe a)
     
     if (!(argc == MAX_ARGS || argc == MAX_ARGS - 2)) {     
@@ -225,11 +243,11 @@ int get_arguments(int argc, const char* argv[], int& k, int& L, float& a, int& R
 
     
     // Get the filename
-    const char* flag_small;      // Flag for siftsmall  
+    const char* data_set_flag;      // Flag for siftsmall  
     arg_found = false;
     for (int i = 1; i <= final_flag; i += 2) {
         if (!strcmp(argv[i], "-f")) {
-            flag_small = argv[i + 1];
+            data_set_flag = argv[i + 1];
             arg_found = true;
         }
     } 
@@ -238,25 +256,36 @@ int get_arguments(int argc, const char* argv[], int& k, int& L, float& a, int& R
         return -1;
     }
 
+    data_set = string(data_set_flag);
     // string base_name;
     // string groundtruth_name;
     // string query_name;
-    if( !strcmp(flag_small, "small") ){
+    if (data_set == "small") {
         base_name =  "sift/siftsmall_base.fvecs";
         query_name = "sift/siftsmall_query.fvecs";
         groundtruth_name = "sift/siftsmall_groundtruth.ivecs";
     }
-    else if( !strcmp(flag_small, "large") ){
+    else if (data_set == "large") {
         base_name =  "sift/sift_base.fvecs";
         query_name = "sift/sift_query.fvecs";
         groundtruth_name = "sift/sift_groundtruth.ivecs";
     }
-    else if( !strcmp(flag_small, "dummy") ){
+    else if (data_set == "dummy") {
         base_name =  "sift/dummy-data.bin";
         query_name = "sift/dummy-queries.bin";
         groundtruth_name = "sift/groundtruth.bin";
     }
-    else{
+    else if (data_set == "contest1m") {
+        base_name =  "contest-data-release-1m.bin";
+        query_name = "contest-queries-release-1m.bin";
+        groundtruth_name = "sift/groundtruth1m.bin";
+    }
+    else if (data_set == "contest10m") {
+        base_name =  "contest-data-release-10m.bin";
+        query_name = "contest-queries-release-10m.bin";
+        groundtruth_name = "sift/groundtruth10m.bin";
+    }
+    else {
         cerr << "ERROR: Should include the file name as command line argument like \"-f small\" or \"-f large\"\n";
         return -1;
     }
@@ -387,7 +416,7 @@ bool hasBinExtension(const string& filename) {
 
 /* Reads queries from sift/dummy-queries.bin */
 /* Returns a map from query nodes to their floating type filter value (-1 for no filter) */
-map<vector<float>, float> read_queries(const string& filename) {
+pair< vector<vector<float>> , vector<float>> read_queries(const string& filename, int num_queries, int num_dimensions) {
     // Check the file extention
     if(!hasBinExtension(filename)) { // !!! SHOULD BE CALLED WITH TRY AND CATCH
         throw invalid_argument("File must have a .bin extention: " + filename);
@@ -404,41 +433,104 @@ map<vector<float>, float> read_queries(const string& filename) {
     // Get number of queries
     uint32_t N;
     ifs.read((char *)&N, sizeof(uint32_t));
-    cout << "# of pueries: " << N << endl;
+    cout << "# of queries: " << N << endl;
 
 
     // Initialise buffer and map
-    vector<float> buff(104);
-    map<vector<float>, float> M;
+    vector<float> buff(num_dimensions + 4);
+    vector<vector<float>> v;                             // Holds all the query vector data
+    vector<float> f(num_queries);                     // Holds all the equivalent query filter data (categorical attributes)
 
-    // Read query data repeatitively
-    while (ifs.read((char *)buff.data(), (104) * sizeof(float))) {
+    // Read query data repetitively
+    int i = 0;
+    while (ifs.read((char *)buff.data(), (num_dimensions + 4) * sizeof(float)) && i < num_queries) {
         
         // Casting and storing query value (not used)
         // int query_value = static_cast<float>(buff[0]);
 
         // Casting and storing the filter (categorical attribute)
         float filter = static_cast<float>(buff[1]);
+        f[i] = filter;
+
 
         /* Ignoring timestamps (buff[2] and buff[3]) */
 
         // Casting query vector data to float
-        vector<float> q(100);
-        for (int d = 0; d < 100; d++) {
-            q[d] = static_cast<float>(buff[d+4]);
+        vector<float> q(num_dimensions);
+        for (int d = 0; d < num_dimensions; d++) {
+            q[d] = static_cast<float>(buff[d + 4]);
         }
 
-        /* ADDING ENTRY TO MAP */
-        M[q] = filter; // filter == -1 for no filter
+        /* ADDING ENTRY TO RETURN VECTOR */
+        // v[i] = q;
+        v.push_back(q);
+        i++;
+        cout << i << endl;
     }
-
 
     // Close file
     ifs.close();
     cout << "Finish Reading Queries" << endl;
 
-    return M;
+
+    return {v, f};
 }
+
+// Return 1 if it succeed, else returns 0
+vector<vector<gIndex>> read_groundtruth(string filename) {
+    
+    // Open file and check if it was opened properly
+    ifstream groundtruth(filename, ios::binary);
+    assert(groundtruth.is_open());
+
+    cout << "Reading Data: " << filename << endl;
+
+
+    // Get number of points
+    uint32_t N;
+    groundtruth.read((char *)&N, sizeof(uint32_t));
+    cout << "# of points: " << N << endl;
+
+    int num_dimensions = 1;
+    // Initialize buffer
+    vector<gIndex> buff(num_dimensions);
+    
+    vector<vector<gIndex>> gt_data;
+
+    for (int i = 0 ; i < (int)N ; i++ ){
+
+        // Read k (number of nearest neighbors for this query)
+        size_t k;
+        groundtruth.read(reinterpret_cast<char*>(&k), sizeof(k));
+
+        cout << "Query " << i + 1 << ": (k neighbors found = " << k << ")"<< endl;
+
+        // Read the vector of dimension k
+        vector<gIndex> nearest_neighbor(k);
+        groundtruth.read(reinterpret_cast<char*>(nearest_neighbor.data()), k * sizeof(float));
+
+        // Print the groundtruth of the query
+        cout << "Groundtruth: ";
+        print_vector(nearest_neighbor);
+        cout << endl;
+
+        // Store the vector in the groundtruthData
+        gt_data.push_back(nearest_neighbor);
+
+
+    }
+
+    // Close file
+    groundtruth.close();
+    cout << "Finish Reading Data" << endl;
+
+
+    return gt_data;
+
+}
+
+
+/*-----------------------------------------------------------------------------USED FOR TESTING-----------------------------------------------------------------------------------------*/
 
 
 
@@ -501,55 +593,100 @@ void make_bin(const string& filename, const vector<vector<T>> vectors) {
 
 
 
-// Return 1 if it succeed, else returns 0
-vector<vector<gIndex>> read_groundtruth(string filename) {
-    
-    // Open file and check if it was opened properly
-    ifstream groundtruth(filename, ios::binary);
-    assert(groundtruth.is_open());
 
-    cout << "Reading Data: " << filename << endl;
+/* Creates a .bin file with the given data vectors that starts with the number of vectors */
+template <typename T>
+void make_queries(const string& filename, const vector<vector<T>> vectors) {
+    // Create/Open file
+    ofstream file(filename, ios::binary);
+    if (!file) {
+        throw runtime_error("Failed to open file for writing: " + filename); // !!! Used with try/catch
+    }
 
+    // First we write the number of vectors as the first entry of the file
+    uint32_t num_vectors = vectors.size();                                     // Assuming all vectors will have the same number of dimensions as the first one
+    file.write(reinterpret_cast<const char*>(&num_vectors), sizeof(num_vectors));
 
-    // Get number of points
-    uint32_t N;
-    groundtruth.read((char *)&N, sizeof(uint32_t));
-    cout << "# of points: " << N << endl;
+    // Write data from vectors to .bin file
+    for (const auto& vector : vectors) {      
+        float query_type = vector[0];                                         // Query Type
+        file.write(reinterpret_cast<const char*>(&query_type), sizeof(float));  
+        
+        float filter = vector[1];                                             // Filter
+        file.write(reinterpret_cast<const char*>(&filter), sizeof(float));
+       
+        float timestamp1 = vector[2];                                          // Timestamp1
+        file.write(reinterpret_cast<const char*>(&timestamp1), sizeof(float)); 
+        
+        float timestamp2 = vector[3];                                          // Timestamp2
+        file.write(reinterpret_cast<const char*>(&timestamp2), sizeof(float)); 
 
-    int num_dimensions = 1;
-    // Initialize buffer
-    vector<gIndex> buff(num_dimensions);
-    
-    vector<vector<gIndex>> gt_data;
-
-    for (int i = 0 ; i < (int)N ; i++ ){
-
-        // Read k (number of nearest neighbors for this query)
-        size_t k;
-        groundtruth.read(reinterpret_cast<char*>(&k), sizeof(k));
-
-        cout << "Query " << i + 1 << ": (k neighbors found = " << k << ")"<< endl;
-
-        // Read the vector of dimension k
-        vector<gIndex> nearest_neighbor(k);
-        groundtruth.read(reinterpret_cast<char*>(nearest_neighbor.data()), k * sizeof(float));
-
-        // Print the groundtruth of the query
-        cout << "Groundtruth: ";
-        print_vector(nearest_neighbor);
-        cout << endl;
-
-        // Store the vector in the groundtruthData
-        gt_data.push_back(nearest_neighbor);
-
-
+        // Write the remaining vector data (excluding the filter)              // Vector
+        file.write(reinterpret_cast<const char*>(&(vector[4])), (vector.size() - 4) * sizeof(T));
     }
 
     // Close file
-    groundtruth.close();
-    cout << "Finish Reading Data" << endl;
+    file.close();
+    cout << "Data written to " << filename << " successfully.\n";
+}
 
 
-    return gt_data;
+/* Method to save the medoid map of the graph in a file <prefix>_medoid_map.bin */
+template <typename F>
+bool save_medoid_map_to_bin(string file_prefix, map<F, gIndex> medoid_map) {
+    string filename = "./bin/" + file_prefix + "_medoid_map.bin";
 
+    // Create/Open file filename_medoid_map.bin
+    ofstream file(filename, ios::binary);
+    if (!file) {
+        cerr << "Error opening file for writing!" << endl;
+        return false;
+    }
+    
+
+    // For each pair of the map
+    for (auto it = medoid_map.begin() ; it != medoid_map.end() ; it++ ) {
+        F filter = it->first;
+        gIndex starting_node = it->second;
+
+        // Save filter
+        file.write(reinterpret_cast<const char*>(&filter), sizeof(filter));
+        // Save starting node
+        file.write(reinterpret_cast<const char*>(&starting_node), sizeof(starting_node)); 
+    }
+
+
+    return true;
+}
+
+
+
+
+/* Method to get the medoid map of the graph from a file <prefix>_medoid_map.bin */
+template <typename F>
+bool get_medoid_map_from_bin(string file_prefix, map<F, gIndex>& medoid_map) {
+    string filename = "./bin/" + file_prefix + "_medoid_map.bin";
+
+    // Create/Open file filename_medoid_map.bin
+    ifstream file(filename, ios::binary);
+    if (!file) {
+        cerr << "Error opening file for reading!" << endl;
+        return false;
+    }
+
+
+    while (file) {
+        F filter;
+        gIndex starting_node;
+
+        // Read the filter
+        file.read(reinterpret_cast<char*>(&filter), sizeof(filter));
+
+        // Read the starting node
+        file.read(reinterpret_cast<char*>(&starting_node), sizeof(starting_node));
+
+        medoid_map[filter] = starting_node;
+    }
+
+    return true;
 }
